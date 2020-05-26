f21b3a893ded32fc8cd9b073ab5e08c5108aa0ff
Shahid Saddique <shahidali6@gmail.com>
2020-05-26 09:15:56 +0500

    Using 'use strict'

--- Section 1 - JS Fundamentals/4. Variable/external.js
+++ Section 1 - JS Fundamentals/4. Variable/external.js
@@ -3,4 +3,45 @@
 //'use strict' use the JavaScript with old behaviour .
 'use strict';
 
+// var ename;
+// ename = 'Sahhid';
+// console.log(ename);
 
+// var job = 'Manager';
+
+// var ename = 'King',
+//     age = 78,
+//     job = 'Salesman';
+
+// console.log(ename);
+// ename = 9898;
+// console.log(ename);
+// console.log(age);
+// console.log(job);
+
+
+// var dept;
+// console.log(dept);
+// console.log(salll)
+
+
+// function SayGreeting()
+// {
+//     var message = 'Hello';
+//     console.log(message);
+// }
+
+// console.log(message);
+// SayGreeting();
+// SayGreeting();
+// SayGreeting();
+
+// var message = 'welcome'
+// function SayGreeting()
+// {
+//     var message = 'Hello';
+//     console.log(message);
+// }
+
+// console.log(message);
+// SayGreeting();
